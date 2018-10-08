369ffaf3b7e47aa53a5057747840c2f9bcbf21a7
marqos12 <marqos111222@gmail.com>
2018-10-08 10:24:16 +0200

    dodanie angulara 2>

---
+++ planerWyjazdu/src/app/app.module.ts
@@ -0,0 +1,24 @@
+ import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+ import {BsDropdownModule, ModalModule, TooltipModule} from "ngx-bootstrap";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
