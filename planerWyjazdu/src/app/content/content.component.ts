f2a7c84925cd0c215e2bb84a560d0d2e0b735d93
marqos12 <marqos111222@gmail.com>
2018-10-10 21:28:16 +0200

    modyfikacja wyswietlania miejsc w tabeli

--- planerWyjazdu/src/app/content/content.component.ts
+++ planerWyjazdu/src/app/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Place} from "../models/place";
+import {Place, DayHours} from "../models/place";
+
 
 @Component({
   selector: 'app-content',
@@ -15,11 +16,73 @@ export class ContentComponent implements OnInit {
 
   ngOnInit() {
 
+    let nazwy = ["Wyższa szkołą robienia hałasu",
+      "Muzeum Gówna",
+      "Politechnika Warszawska",
+      "Rzeszów",
+      "Obwodnica Brzozowa",
+      "Dworzec PKP w Brzozowie"];
+
+    let adresy=["ul. Wypizdowska 14/3",
+      "ul. Sraczkowa 9",
+      "ul. Grójecka 134",
+      "al. Raka 2000",
+      "ul. Szpitalna",
+      "al. Obok Biedronki"];
+
+    let godzinyOtwarcia: number[]=[9,11,7,8,10,7]
+    let minutyOtwarcia:number[]=[30,30,0,0,0,30];
+    let godzinyZamkniecia=[14,18,14,16,15,15];
+    let minutyZamkniecia:number[]=[0,0,30,0,30,30];
+
+    let godzinyOtwarciaS=[11,9,9,8,10,9]
+    let minutyOtwarciaS:number[]=[0,0,0,0,0,0];
+    let godzinyZamknieciaS=[14,15,14,16,17,18];
+    let minutyZamknieciaS:number[]=[0,0,0,0,30,30];
+
+
     for (let i = 0; i < 6; i++){
 
       let place = new Place();
-      place.name="Stopy Stanisława";
-      place.address="Stanisławowo Wielkie pod Radzymiem 2/5";
+
+      place.name=nazwy[i];
+      place.address=adresy[i];
+
+      place.monday.startHours=godzinyOtwarcia[i];
+      place.monday.startMinutes=minutyOtwarcia[i];
+      place.monday.stopHours=godzinyZamkniecia[i];
+      place.monday.stopMinutes=minutyZamkniecia[i];
+
+      place.tuesday.startHours=godzinyOtwarcia[i];
+      place.tuesday.startMinutes=minutyOtwarcia[i];
+      place.tuesday.stopHours=godzinyZamkniecia[i];
+      place.tuesday.stopMinutes=minutyZamkniecia[i];
+
+      place.wednesday.startHours=godzinyOtwarcia[i];
+      place.wednesday.startMinutes=minutyOtwarcia[i];
+      place.wednesday.stopHours=godzinyZamkniecia[i];
+      place.wednesday.stopMinutes=minutyZamkniecia[i];
+
+      place.thursday.startHours=godzinyOtwarcia[i];
+      place.thursday.startMinutes=minutyOtwarcia[i];
+      place.thursday.stopHours=godzinyZamkniecia[i];
+      place.thursday.stopMinutes=minutyZamkniecia[i];
+
+      place.friday.startHours=godzinyOtwarcia[i];
+      place.friday.startMinutes=minutyOtwarcia[i];
+      place.friday.stopHours=godzinyZamkniecia[i];
+      place.friday.stopMinutes=minutyZamkniecia[i];
+
+      place.saturday.startHours=godzinyOtwarciaS[i];
+      place.saturday.startMinutes=minutyOtwarciaS[i];
+      place.saturday.stopHours=godzinyZamknieciaS[i];
+      place.saturday.stopMinutes=minutyZamknieciaS[i];
+
+      place.sunday.startHours=godzinyOtwarcia[i];
+      place.sunday.startMinutes=minutyOtwarcia[i];
+      place.sunday.stopHours=godzinyZamkniecia[i];
+      place.sunday.stopMinutes=minutyZamkniecia[i];
+
       this.places.push(place);
 
     }
--- planerWyjazdu/src/app/content/place-body/place-body.component.ts
+++ planerWyjazdu/src/app/content/place-body/place-body.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Place} from "../../models/place";
+import {DomSanitizer, SafeStyle} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-place-body',
@@ -11,6 +12,9 @@ export class PlaceBodyComponent implements OnInit {
   public place: Place;
   public id: number;
 
+  private spaceWidth:number;
+
+
   @Input() set setPlace (place: Place) {
     this.place = place;
   }
@@ -18,12 +22,22 @@ export class PlaceBodyComponent implements OnInit {
     this.id = id;
   }
 
-
   constructor() {
 
   }
 
   ngOnInit() {
+    this.spaceWidth = (window.innerWidth-20)*0.58;
+    console.log(this.spaceWidth);
   }
+
+
+
+
+  getRandomColor() {
+    return 'rgb('+Math.floor(Math.random() * 256)+','+Math.floor(Math.random() * 256)+','+Math.floor(Math.random() * 256)+');';
+  }
+
+
 
 }
