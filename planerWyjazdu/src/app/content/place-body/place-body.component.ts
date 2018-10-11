5dcd5d0f860b4b4e125383c1801344098ff069ac
marqos12 <marqos111222@gmail.com>
2018-10-11 20:30:00 +0200

    Zmiany w klasie miejsca oraz wyswietlaniu miejsca w tabeli

--- planerWyjazdu/src/app/content/place-body/place-body.component.ts
+++ planerWyjazdu/src/app/content/place-body/place-body.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Place} from "../../models/place";
+import {DayHours, Place} from "../../models/place";
 import {DomSanitizer, SafeStyle} from "@angular/platform-browser";
+import {PlacesServiceService} from "../../service/places-service.service";
 
 @Component({
   selector: 'app-place-body',
@@ -11,9 +12,11 @@ export class PlaceBodyComponent implements OnInit {
 
   public place: Place;
   public id: number;
+  public openHoours: string;
+
 
   private spaceWidth:number;
-
+  private hourWidth:number;
 
   @Input() set setPlace (place: Place) {
     this.place = place;
@@ -22,17 +25,68 @@ export class PlaceBodyComponent implements OnInit {
     this.id = id;
   }
 
-  constructor() {
+  constructor(private placeService: PlacesServiceService) {
 
   }
 
   ngOnInit() {
     this.spaceWidth = (window.innerWidth-20)*0.58;
-    console.log(this.spaceWidth);
+    this.hourWidth = this.spaceWidth / this.placeService.getHourRange();
+
   }
 
 
+  checkOpenHours(){
+    this.openHoours="pn";
+    let isTheSame:boolean=false;
+    for (let i = 1; i < 7; i++){
+      if((this.place.days[i].startHours == this.place.days[i-1].startHours)
+        &&(this.place.days[i].startMinutes == this.place.days[i-1].startMinutes)
+        &&(this.place.days[i].stopHours == this.place.days[i-1].stopHours)
+        &&(this.place.days[i].stopMinutes == this.place.days[i-1].stopMinutes)){
+        //jeżeli jest takie samo jak poprzedni dzien
+        isTheSame=true;
+      }
+      else {
+        //jeżeli nie jest takie samo jak poprzedni dzień
+        if(isTheSame){
+          //ale poprzedni dzień był taki sam jak wcześniejszy
+          this.openHoours+="-"+this.getDayShortcut(i-1)+" "+this.getTimeFromDay(this.place.days[i-1]);
+          isTheSame = false;
+        }
+        else{
+          //ale poprzedni dzień nie był taki sam jak wczesniejszy
 
+        }
+      }
+    }
+  }
+
+  private getDayShortcut(i:number){
+    switch (i) {
+      case 1:
+        return "pn";
+      case 2:
+        return "wt";
+      case 3:
+        return "sr";
+      case 4:
+        return "czw";
+      case 5:
+        return "pt";
+      case 6:
+        return "so";
+      case 7:
+        return "nd";
+    }
+  }
+
+  private getTimeFromDay(day:DayHours):string{
+    return day.startHours.toLocaleString()+":"
+      +day.startMinutes.toLocaleString()+"-"
+      +day.stopHours.toLocaleString()+":"
+      +day.stopMinutes.toLocaleString();
+  }
 
   getRandomColor() {
     return 'rgb('+Math.floor(Math.random() * 256)+','+Math.floor(Math.random() * 256)+','+Math.floor(Math.random() * 256)+');';
--- planerWyjazdu/src/app/models/place.ts
+++ planerWyjazdu/src/app/models/place.ts
@@ -5,13 +5,22 @@ export class Place {
   name: string;
   address: string;
 
-  monday:DayHours=new DayHours;
+  /*monday:DayHours=new DayHours;
   tuesday: DayHours=new DayHours;
   wednesday: DayHours=new DayHours;
   thursday: DayHours=new DayHours;
   friday: DayHours=new DayHours;
   saturday: DayHours=new DayHours;
-  sunday: DayHours=new DayHours;
+  sunday: DayHours=new DayHours;*/
+  days:DayHours[]=[];
+
+  constructor(){
+
+    for (let i = 0; i < 7; i++){
+      this.days.push(new DayHours());
+    }
+  }
+
 }
 
 export class DayHours{
@@ -20,4 +29,5 @@ export class DayHours{
   startMinutes: number;
   stopHours: number;
   stopMinutes:number;
+  color: string = "#a10367";
 }
